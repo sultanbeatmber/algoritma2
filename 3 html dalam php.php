937107baeffc514cd3a5f9fdcbfec3871c998e92
sultanbeatmber <fauzansultan656@gmail.com>
2024-08-27 13:25:05 +0700

    3 html dalam php

---
+++ 3 html dalam php.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<html>";
+echo "<head>";
+echo "<title>Judul Web</title>";
+echo "</head>";
+echo "<body>";
+echo "<p><h1>Selamat datang</h1><p>";
+echo "<p>ini adalah contoh file html di dalam tag php</p>";
+echo "<p>by `SULTAN GENK</p>";
+echo "</body>";
+echo "</html>";
