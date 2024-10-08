3b91f34e55ed8dfc1267faa1699fbfefe02ff134
sultanbeatmber <fauzansultan656@gmail.com>
2024-10-08 11:51:20 +0700

    Create menghapus variabel.php

---
+++ menghapus variabel.php
@@ -0,0 +1,11 @@
+<?php
+// membuat variabel $tmp
+$tmp = 2901;
+
+// menghapus variabel $tmp
+// unset($tmp);
+
+// mencoba mengakses variabel $tmp
+echo "<h1>". $tmp;
+
+// PHP Notice:  Undefined variable: tmp
