5b6af9336965044be5842135d0ddd4718a0bae9c
sultanbeatmber <fauzansultan656@gmail.com>
2024-10-08 10:46:14 +0700

    Create 09 mencetak dengan echo.php
    merubah no file&update mencetak dengan echo

---
+++ 09 mencetak dengan echo.php
@@ -0,0 +1,23 @@
+<?php
+
+// membuat variabel baru
+$nama_barang = "kaliper nissin";
+$harga = 15000;
+$tempat = "kuala simpang";
+
+// menampilkan isi variabel
+echo "sultanbeatmber membeli $nama_barang seharga Rp $harga di $tempat <br>";
+
+// Jika kita menggunakan tanda petik ganda (") dalam echo
+$judul = "Belajar PHP dari nol sampai expert";
+echo "Judul artikel: $judul";
+
+//Apabila kita menggunakan tanda petik tunggal ('), maka kita harus menggunakan titik untuk menggabungkan teks dengan variabelnya
+$judul = "Tutorial PHP untuk Pemula";
+echo 'Judul artikel: '.$judul;
+
+//Fungi echo() adalah fungsi untuk menampilkan teks ke layar. Fungsi ini dapat digunakan dengan tanda kurung maupun tanpa tanda kurung
+echo "<h2>Belajar PHP itu mudah!</h2>";
+echo("Hello world!<br>");
+echo "Disedang belajar PHP!<br>";
+echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah.";
